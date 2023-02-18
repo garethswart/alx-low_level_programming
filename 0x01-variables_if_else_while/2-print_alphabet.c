378ab5c05bc86dee28fa199b4daae1945b9b5f44
Gareth Swart <garethswart@gmail.com>
2023-02-18 03:42:04 -0800

    corrected newlines and training whitespace in files 0-2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 int main(void)
 {
 	char letters[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
-	
 	while (i < 26)
 	{
 		putchar (letters[i]);
