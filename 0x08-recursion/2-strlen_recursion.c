3012d7797364fcdf49bc5b7eadd1b070fac6276c
Gareth Swart <garethswart@gmail.com>
2023-03-08 18:02:19 -0800

    Added 0x08 dir, main.h, readme, and files with main.c files from 0 - 4

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be measured.
+ *
+ * Return: length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	_putchar(_print_rev_recursion(s[1]));
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed.
+ *
+ * Return: void.
+ */
+void _puts_recursion(char *s);
+
+/**
+ * _print_rev_recursion - prints a string, in reverse.
+ * @s: string to be printed.
+ *
+ * Return: void.
+ */
+void _print_rev_recursion(char *s);
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to be measured.
+ *
+ * Return: length of string.
+ */
+int _strlen_recursion(char *s);
+
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: number.
+ *
+ * Return: factorial of number.
+ */
+int factorial(int n);
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: number to be powered.
+ * @y: exponent.
+ *
+ * Return: power.
+ */
+int _pow_recursion(int x, int y);
+
+#endif
