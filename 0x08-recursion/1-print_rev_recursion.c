37bebf361b548d92388b2001c61995068c56ede5
Gareth Swart <garethswart@gmail.com>
2023-03-09 04:56:43 -0800

    Fixed pointer issue in file 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
-	_putchar(_print_rev_recursion(s[1]));
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
