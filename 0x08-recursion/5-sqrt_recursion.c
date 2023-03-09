7068799ca8fab700b449b44765e8ff7877834ff1
Gareth Swart <garethswart@gmail.com>
2023-03-09 09:20:06 -0800

    Completed file 5 and added main.c for 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _sqrt - returns the square root of a number.
+ * @n: number to be rooted.
+ * @i: index.
+ *
+ * Return: sqare root.
+ */
+int _sqrt(int n, int i)
+{
+	if (i < 0)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+		return (i);
+	else 
+		return (_sqrt(n, i - 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to be rooted.
+ *
+ * Return: sqare root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return (_sqrt(n, n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -50,4 +50,12 @@ int factorial(int n);
  */
 int _pow_recursion(int x, int y);
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to be rooted.
+ *
+ * Return: sqare root.
+ */
+int _sqrt_recursion(int n);
+
 #endif
