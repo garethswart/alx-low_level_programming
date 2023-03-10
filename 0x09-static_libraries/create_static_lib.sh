1f29a12eb1cd216c247eec74bb5f13cbcb5dc52d
Gareth Swart <garethswart@gmail.com>
2023-03-10 06:14:28 -0800

    simplified file names in create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# Get the current directory
-dir=$(pwd)
-
-# Set the library name
-libname="liball.a"
-
-# Create an empty library file
-touch $liball
-
 # Loop through all the C files in the directory
-for file in $dir/*.c
+for file in *.c
 do
   # Compile the file and create an object file
   gcc -c $file -o ${file%.c}.o
 
   # Add the object file to the library
-  ar rcs $liball ${file%.c}.o
+  ar rcs liball.a ${file%.c}.o
 done
