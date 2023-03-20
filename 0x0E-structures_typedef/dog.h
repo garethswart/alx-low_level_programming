6324692a2eaa9ccc9eacdd6e7f14c83adab518cd
Gareth Swart <garethswart@gmail.com>
2023-03-20 12:48:35 -0700

    Added file 4 with 4-main.c test file, added negative number handler in file 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -37,4 +37,14 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  */
 void print_dog(struct dog *d);
 
+/**
+ * new_dog - initiates dog_t details
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Return: pointer to new dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
