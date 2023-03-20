fc5d2216c964eb77a6a491a38c77dbf8ebacd637
Gareth Swart <garethswart@gmail.com>
2023-03-20 08:45:29 -0700

    Added dir for 0x0E and added file 0 and its main.c file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog details
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
